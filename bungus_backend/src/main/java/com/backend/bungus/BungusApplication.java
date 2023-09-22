ccd132a9d66475448dc5085dd4a7e05d18b065f8
SuprHUlk <ayush123kumar.ak@gmail.com>
2023-09-22 21:38:08 +0530

    Changed file structure

---
+++ bungus_backend/src/main/java/com/backend/bungus/BungusApplication.java
@@ -0,0 +1,13 @@
+package com.backend.bungus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BungusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BungusApplication.class, args);
+	}
+
+}
