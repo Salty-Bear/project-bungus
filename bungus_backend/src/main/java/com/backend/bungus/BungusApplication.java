fb32b8f10a51242e67f4046760f4ad9e41c000e5
SuprHUlk <ayush123kumar.ak@gmail.com>
2023-09-22 21:57:27 +0530

    Initialized firebase

--- bungus_backend/src/main/java/com/backend/bungus/BungusApplication.java
+++ bungus_backend/src/main/java/com/backend/bungus/BungusApplication.java
@@ -1,12 +1,37 @@
 package com.backend.bungus;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class BungusApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		ClassLoader classLoader = BungusApplication.class.getClassLoader();
+
+		File file =
+				new File(Objects.requireNonNull(classLoader.getResource("serviceKey.json")).getFile());
+
+		FileInputStream serviceAccount =
+				new FileInputStream(file.getAbsolutePath());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+
+		FirebaseApp.initializeApp(options);
+
+
 		SpringApplication.run(BungusApplication.class, args);
 	}
 
