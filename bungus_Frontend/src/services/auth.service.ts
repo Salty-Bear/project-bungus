33a761e2a3b31f3e10ff0cedcc9b47f04d6c3aef
Aryaman <aryamanraj123456789@gmail.com>
2023-09-23 12:37:02 +0530

    ok

--- bungus_Frontend/src/services/auth.service.ts
+++ bungus_Frontend/src/services/auth.service.ts
@@ -1,13 +1,54 @@
 import { Injectable } from '@angular/core';
 // import { AngularFireAuth} from
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { GoogleAuthProvider, GithubAuthProvider } from '@angular/fire/auth';
+import { BehaviorSubject} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServicesService {
+export class AuthService {
 
-  constructor() { }
+  constructor(private fireauth: AngularFireAuth,private router : Router) { }
+
+  isUserLoggedIn = new BehaviorSubject<boolean>(false)
+
+/* ############################################################
+  Creating Sign in with google service for logging in
+ ############################################################ */
+
+  googleSignIn(){
+    return this.fireauth.signInWithPopup(new GoogleAuthProvider).then(
+      res =>{
+        this.router.navigate(['/dashboard'])
+        localStorage.setItem('token',JSON.stringify(res.user?.uid))
+        this.isUserLoggedIn.next(true)
+      },
+      err =>{
+        alert(err.message)
+      }
+      )
+    }
 
 
-}
+/* ############################################################
+  Creating Sign in with Github service for logging in
+ ############################################################ */
+
+    githubSignIn(){
+      return this.fireauth.signInWithPopup(new GithubAuthProvider).then(
+        res => {
+          this.router.navigate(['/dashboard'])
+          localStorage.setItem('token',JSON.stringify(res.user?.uid))
+          
+        },
+        err =>{
+          alert(err.message)
+        }
+      )
+    }
+
+
+  }
+
