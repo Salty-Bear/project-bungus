ac6f100b64fe1f1579512d184a72e8ccdb06ece6
Aryaman <aryamanraj123456789@gmail.com>
2023-09-23 15:40:48 +0530

    frontend

--- bungus_Frontend/src/app/app.module.ts
+++ bungus_Frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -31,6 +31,11 @@ import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+
+
+
+
+ 
 
 
 @NgModule({
