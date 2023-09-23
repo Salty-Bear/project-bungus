54eb3d1ee361b8a43f6a70a9553a45ec7029f09c
Aryaman <aryamanraj123456789@gmail.com>
2023-09-23 08:11:12 +0530

    authguard

---
+++ bungus_Frontend/src/app/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class authGuard {
+  constructor(private auth:AuthService,private router:Router){}
+
+  isAuth=false;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.auth.isUserLoggedIn
+  }
+  
+}
